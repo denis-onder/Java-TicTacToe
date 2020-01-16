cfe59c5df043fd1c75b169d6589f713ff16e47d5
Denis Onder <denis_onder@protonmail.com>
2020-01-16 06:20:06 +0100

    Logging index

--- src/tictactoe/GameLogic.java
+++
@@ -1,9 +0,0 @@
-package tictactoe;
-
-import javax.swing.*;
-
-public class GameLogic {
-    public static void handleClick(JButton btn) {
-        btn.setText("X");
-    }
-}
--- src/tictactoe/GameScreen.java
+++ src/tictactoe/GameScreen.java
@@ -10,13 +10,16 @@ public class GameScreen extends JFrame {
 
     private JPanel root;
 
+    private JButton[] buttons = new JButton[NUMBER_OF_BUTTONS];
+
     public GameScreen() {
+        // Config
         setResizable(false);
         setSize(400,400);
         root.setLayout(new GridLayout(3, 3));
-
+        // Draw buttons
         drawButtons();
-
+        // Add root panel to frame
         add(root);
 
     }
@@ -24,21 +27,33 @@ public class GameScreen extends JFrame {
     private void drawButtons() {
         for (int i = 0; i < NUMBER_OF_BUTTONS; i++) {
             // Create new JButton
-            JButton btn = new JButton();
+            JButton btn = new JButton(" ");
             // Styles
             btn.setBackground(root.getBackground());
             btn.setBorder(BorderFactory.createLineBorder(Color.white));
             btn.setForeground(Color.white);
+            btn.setFont(new Font("Arial", Font.PLAIN, 40));
             // Attach click listener to button
+            int finalI = i;
             btn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    GameLogic.handleClick(btn);
+                    handleClick(btn, finalI);
                 }
             });
-            // Add button to grid
+            // Add button to grid and the buttons array
             root.add(btn);
+            buttons[i] = btn;
         }
     }
-
+    private void computerMove() {
+        for (JButton btn:buttons) {
+            System.out.println(btn);
+        }
+    }
+    private void handleClick(JButton btn, int index) {
+        btn.setText("X");
+        System.out.println(index);
+//        computerMove();
+    }
 }
